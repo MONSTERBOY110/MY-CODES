13167acb49964430896c6281d63649736fd99d91
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2025-12-29 12:08:56 +0530

    Create 29.12.2025.cpp

---
+++ POTD/GeeksForGeeks/29.12.2025.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
+        // code here
+        int n= a.size(),m = b.size(),i =0,j=0;
+        while(i<n && j<m){
+            if(a[i] < b[j]){
+                if(k==1)
+                    return a[i];
+                i++;k--;
+            }
+            else{
+                if(k==1)
+                    return b[j];
+                j++;k--;
+            }
+        }
+        while(i<n){
+            if(k==1)
+                return a[i];
+            i++;k--;
+        }
+        while(j<m){
+            if(k==1)
+                return b[j];
+            j++;k--;
+        }
+    }
+};
