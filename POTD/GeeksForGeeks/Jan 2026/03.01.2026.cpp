4ed94687fa038a1c7b7947b01e83b657154ce087
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2026-01-03 17:10:06 +0530

    Create 03.01.2026.cpp

---
+++ POTD/GeeksForGeeks/Jan 2026/03.01.2026.cpp
@@ -0,0 +1,41 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* bottom;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+};
+*/
+struct cmp{
+    bool operator()(Node* n1,Node* n2)
+    {
+        return n1->data > n2->data;
+    }
+};
+class Solution {
+  public:
+    Node *flatten(Node *root) {
+        // code here
+        priority_queue<Node*,vector<Node*>,cmp> minheap;
+        for(Node* p=root;p;p=p->next)
+            minheap.push(p);
+        Node* newList = new Node(-1);
+        Node* ptr = newList;
+        while(!minheap.empty())
+        {
+            Node* node = minheap.top();
+            minheap.pop();
+            ptr->bottom = node;
+            ptr = ptr->bottom;
+            if(node->bottom)
+                minheap.push(node->bottom);
+        }
+        return newList->bottom;
+    }
+};
