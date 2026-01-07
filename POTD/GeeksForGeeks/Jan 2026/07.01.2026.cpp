7927e4d6bfd1a93e6b6270f4e78e0b6d222826fd
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2026-01-07 11:49:46 +0530

    Add countDistinct function for distinct element count
    Implement countDistinct function to count distinct elements in sliding window.

---
+++ POTD/GeeksForGeeks/Jan 2026/07.01.2026.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> countDistinct(vector<int> &arr, int k) {
+        // code here
+        int i=0;
+        vector<int> res;
+        unordered_map<int,int> mp;
+        for(int j=0;j<arr.size();j++){
+            mp[arr[j]]++;
+            if(j-i+1==k){
+               res.push_back(mp.size());
+               mp[arr[i]]--;
+               if(mp[arr[i]]==0)
+                 mp.erase(arr[i]);
+               i++;
+            }
+        }
+        return res;
+    }
+};
