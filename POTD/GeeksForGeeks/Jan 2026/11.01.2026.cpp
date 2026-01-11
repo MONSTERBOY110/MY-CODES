345041723dfc56661e1eac994a11c7181730ca68
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2026-01-11 11:55:46 +0530

    Create 11.01.2026.cpp

---
+++ POTD/GeeksForGeeks/Jan 2026/11.01.2026.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    string minWindow(string& s1, string& s2) {
+        // Code here
+        int i=0,j=0,n=s1.length(),m=s2.length(),len=INT_MAX;
+        string ans = "";
+        while (i < n) {
+            if (s1[i] == s2[j]) 
+              j++;
+            if (j == m) {
+                int end = i;
+                j = m - 1;
+                while (i >= 0 && j >= 0) {
+                    if (s1[i] == s2[j]) j--;
+                    i--;
+                }
+                i++;
+                if (end - i + 1 < len) {
+                    len = end - i + 1;
+                    ans = s1.substr(i, len);
+                }
+                j = 0;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
