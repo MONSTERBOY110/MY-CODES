d5036d58d6ab4a2289e5e277a1cb4b96ef50a3ac
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2026-01-12 12:09:23 +0530

    Create 12.01.2026.cpp

---
+++ POTD/GeeksForGeeks/Jan 2026/12.01.2026.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size();
+        deque<int> dq;
+        vector<int> ans;
+        for (int i = 0; i < k; i++) {
+            while (!dq.empty() && arr[dq.back()] < arr[i])
+                dq.pop_back();
+            dq.push_back(i);
+        }
+        ans.push_back(arr[dq.front()]);
+        for (int i = k; i < n; i++) {
+            if (!dq.empty() && i-dq.front() >= k)
+                dq.pop_front();
+            while (!dq.empty() && arr[dq.back()] < arr[i])
+                dq.pop_back();
+            dq.push_back(i);
+            ans.push_back(arr[dq.front()]);
+        }
+        return ans;
+    }
+};
