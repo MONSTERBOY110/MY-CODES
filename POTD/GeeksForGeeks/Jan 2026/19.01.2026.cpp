8bb5511a9fc615de03986552b81d95994a3abfe5
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2026-01-19 12:55:54 +0530

    Create 19.01.2026.cpp

---
+++ POTD/GeeksForGeeks/Jan 2026/19.01.2026.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    string removeKdig(string &s, int k) {
+        // code here
+        stack<char>st;
+        string ans="";
+        int count=0;
+        for(auto & i:s){
+            while(!st.empty() && st.top()>i && count<k){
+                st.pop();
+                count++;
+            }
+            if(st.empty() && i=='0')continue;
+            st.push(i);
+        }
+        while(!st.empty() && count<k){
+            st.pop();
+            count++;
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        if(ans.size()==0) return "0";
+        return ans;
+    }
+};
