4e61d78ba7167005e77d10d587416c99156db523
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2026-04-20 11:48:59 +0530

    Update knapsack.c

--- College Lab DAA/knapsack.c
+++ College Lab DAA/knapsack.c
@@ -1,67 +1,37 @@
 #include <stdio.h>
 
-void knapsack() {
-    float weight[] = {10, 20, 30};
-    float profit[] = {60, 100, 120};
-    int n = 3;
-    float capacity = 50;
-    
-    // Calculate ratios
-    float ratio[n];
-    for (int i = 0; i < n; i++)
-        ratio[i] = profit[i] / weight[i];
-    
-    // Sort by ratio (bubble sort for simplicity)
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (ratio[j] < ratio[j + 1]) {
-                // Swap ratios
-                float temp = ratio[j];
-                ratio[j] = ratio[j + 1];
-                ratio[j + 1] = temp;
-                
-                // Swap weights
-                temp = weight[j];
-                weight[j] = weight[j + 1];
-                weight[j + 1] = temp;
-                
-                // Swap profits
-                temp = profit[j];
-                profit[j] = profit[j + 1];
-                profit[j + 1] = temp;
-            }
-        }
-    }
-    
-    float total_profit = 0;
-    float total_weight = 0;
-    
-    printf("Knapsack (Capacity: %.2f):\n", capacity);
-    printf("Weight\tProfit\tRatio\n");
-    
-    for (int i = 0; i < n; i++) {
-        printf("%.2f\t%.2f\t%.2f\n", weight[i], profit[i], ratio[i]);
-        
-        if (total_weight + weight[i] <= capacity) {
-            total_weight += weight[i];
-            total_profit += profit[i];
-            printf("  -> Added to knapsack\n");
-        } else {
-            float remaining = capacity - total_weight;
-            if (remaining > 0) {
-                float fraction = remaining / weight[i];
-                total_profit += fraction * profit[i];
-                printf("  -> Added %.2f fraction to knapsack\n", fraction);
-                break;
-            }
-        }
-    }
-    
-    printf("\nTotal weight: %.2f\n", total_weight);
-    printf("Total profit: %.2f\n", total_profit);
-}
-
 int main() {
-    knapsack();
+    int val[20], wt[20];
+    int n, cap;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d %d", &val[i], &wt[i]);
+
+    scanf("%d", &cap);
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if((float)val[i]/wt[i] < (float)val[j]/wt[j]) {
+                int t = val[i]; val[i] = val[j]; val[j] = t;
+                t = wt[i]; wt[i] = wt[j]; wt[j] = t;
+            }
+        }
+    }
+
+    float total = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(wt[i] <= cap) {
+            total += val[i];
+            cap -= wt[i];
+        } else {
+            total += (float)val[i]/wt[i] * cap;
+            break;
+        }
+    }
+
+    printf("%.2f", total);
     return 0;
 }
