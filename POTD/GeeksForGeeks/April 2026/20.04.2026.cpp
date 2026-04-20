3cd81169629b03fb38d7b4626dfb5bcf9ee5bb2b
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2026-04-20 20:54:57 +0530

    Create 20.04.2026.cpp

---
+++ POTD/GeeksForGeeks/April 2026/20.04.2026.cpp
@@ -0,0 +1,9 @@
+if(n==1) return 0;
+        if(n==2) return 1;
+        int x=0,y=1;
+        for(int i=3;i<=n;i++){
+            int p=(i-1)*(x+y);
+            x=y;
+            y=p;
+        }
+        return y;
