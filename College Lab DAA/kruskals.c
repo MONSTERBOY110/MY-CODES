8982d33341a9a26d80d2859a716e436dfecb3dbe
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2026-04-27 10:36:29 +0530

    Update kruskals.c

--- College Lab DAA/kruskals.c
+++ College Lab DAA/kruskals.c
@@ -1,86 +1,48 @@
 #include <stdio.h>
-
-#define MAX_V 6
-#define MAX_E 9
-
-int parent[MAX_V];
-int rank_arr[MAX_V];
-
+#define MAX 20
+int parent[MAX];
 int find(int i) {
-    if (parent[i] != i)
-        parent[i] = find(parent[i]);
-    return parent[i];
+    while (parent[i] != i)
+        i = parent[i];
+    return i;
 }
-
-void unionSet(int x, int y) {
-    int xroot = find(x);
-    int yroot = find(y);
-    
-    if (rank_arr[xroot] < rank_arr[yroot])
-        parent[xroot] = yroot;
-    else if (rank_arr[xroot] > rank_arr[yroot])
-        parent[yroot] = xroot;
-    else {
-        parent[yroot] = xroot;
-        rank_arr[xroot]++;
-    }
-}
-
-void kruskals(int u[], int v[], int weight[], int num_edges, int num_vertices) {
-    // Sort edges by weight (bubble sort)
-    for (int i = 0; i < num_edges - 1; i++) {
-        for (int j = 0; j < num_edges - i - 1; j++) {
-            if (weight[j] > weight[j + 1]) {
-                // Swap weights
-                int temp = weight[j];
-                weight[j] = weight[j + 1];
-                weight[j + 1] = temp;
-                
-                // Swap u vertices
-                temp = u[j];
-                u[j] = u[j + 1];
-                u[j + 1] = temp;
-                
-                // Swap v vertices
-                temp = v[j];
-                v[j] = v[j + 1];
-                v[j + 1] = temp;
+void kruskals(int n, int graph[MAX][MAX]) {
+    int total = 0, count = 0;
+    for (int i = 0; i < n; i++)
+        parent[i] = i;
+    printf("Minimum Spanning Tree:\n");
+    printf("Edge\tWeight\n");
+    while (count < n - 1) {
+        int min = 999, u = -1, v = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (graph[i][j] != 0 && graph[i][j] < min) {
+                    min = graph[i][j];
+                    u = i;
+                    v = j;
+                }
             }
         }
-    }
-    
-    for (int i = 0; i < num_vertices; i++) {
-        parent[i] = i;
-        rank_arr[i] = 0;
-    }
-    
-    printf("Minimum Spanning Tree (Kruskal's Algorithm):\n");
-    printf("Edge\tWeight\n");
-    
-    int e = 0;
-    int total_weight = 0;
-    
-    for (int i = 0; e < num_vertices - 1 && i < num_edges; i++) {
-        int x = find(u[i]);
-        int y = find(v[i]);
-        
-        if (x != y) {
-            printf("%d-%d\t%d\n", u[i], v[i], weight[i]);
-            total_weight += weight[i];
-            unionSet(x, y);
-            e++;
+        int a = find(u);
+        int b = find(v);
+        if (a != b) {
+            printf("%d - %d\t%d\n", u, v, min);
+            total += min;
+            parent[a] = b;
+            count++;
         }
+        graph[u][v] = graph[v][u] = 0;
     }
-    
-    printf("\nTotal weight: %d\n", total_weight);
+    printf("\nTotal weight: %d\n", total);
 }
-
 int main() {
-    int u[] = {0, 0, 1, 1, 2, 2, 3, 3, 4};
-    int v[] = {1, 2, 2, 3, 3, 4, 4, 5, 5};
-    int weight[] = {4, 2, 1, 5, 8, 10, 2, 6, 3};
-    
-    kruskals(u, v, weight, MAX_E, MAX_V);
-    
+    int n, graph[MAX][MAX];
+    printf("Enter No of Vertices: ");
+    scanf("%d", &n);
+    printf("Enter the adjacency matrix (%d x %d):\n", n, n);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &graph[i][j]);
+    kruskals(n, graph);
     return 0;
-}
+}                
