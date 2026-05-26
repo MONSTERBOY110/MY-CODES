1515a095b68141e48a480298a018928b71b56d93
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2026-05-26 11:24:05 +0530

    Create 26.05.2026.cpp

---
+++ POTD/GeeksForGeeks/May 2026/26.05.2026.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int minToggle(vector<int>& arr) {
+        // code here
+        int rightZeros=0;
+        for(int num : arr)
+            if (num == 0)
+                rightZeros++;
+        int ans=rightZeros,leftOnes=0;
+        for(int num : arr) {
+            if (num == 0)
+                rightZeros--;
+            else
+                leftOnes++;
+            ans = min(ans, leftOnes + rightZeros);
+        }
+        return ans;
+    }
+};
