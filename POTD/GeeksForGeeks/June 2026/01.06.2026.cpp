8182b07ac95f391e0a634f3e951f8343e9fc403f
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2026-06-01 21:08:09 +0530

    Create 01.06.2026.cpp

---
+++ POTD/GeeksForGeeks/June 2026/01.06.2026.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int findMaxProduct(vector<int>& arr) {
+        // code here
+        int n=arr.size(),mini=1e9, nCnt=0, pCnt=0, zero=0, mod=1e9+7;
+        long long pos=1, neg=1;
+        for(int i=0; i<n; i++){
+            if(arr[i]>0){
+                pos=(pos*arr[i])%mod;
+                pCnt++;
+            }else if(arr[i]<0){
+                neg=(neg*abs(arr[i]))%mod;
+                mini=min(mini, abs(arr[i]));
+                nCnt++;
+            }else zero++;
+        }
+        if(pCnt==0 && nCnt>0){
+            if(nCnt==1){
+                if(zero>0) return 0;
+                else return(-1)*neg;
+            }
+            if((nCnt%2)==1){
+                neg/=mini;
+            }
+            return neg;
+        }else if(pCnt==0 && nCnt==0) return 0;
+        
+        if((nCnt%2)==1)
+            neg/=mini;
+        return (pos*neg)%mod;
+    }
+};
